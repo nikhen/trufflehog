2fe84a6d5d07a6865df250f012b2fc46e67dfa73
nikhen <niko@heners-net.de>
2020-02-02 07:48:10 -0500

    - Added count to display number of repositories - Added zilliqa codebase to repositories to be scanned - minor code beautification

--- trufflehog.sh
+++ trufflehog.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 function scan_repo() {
-    echo "Scanning repository: " $1
+    echo "Scanning repository:" $1
     time trufflehog --regex --entropy=False $1
     echo ""
-    echo "Done. Scanned repository " $1
+    echo "Done. Scanned repository" $1
     echo ""
 }
 
-function main() {
-    echo "Starting repository scan"
-    a_url=(
+function print_separator() {
+    echo "----"
+}
+
+function iterate_over_repos() {
+    local i=0
+    local a_url=(
         'https://github.com/Zilliqa/savant-ide'
         'https://github.com/Zilliqa/scilla'
         'https://github.com/Zilliqa/Zilliqa'
@@ -18,10 +22,24 @@ function main() {
         'https://github.com/cryptolandtech/moonlet-core/releases/tag/v0.0.1'
         'https://github.com/Zilliqa/nucleus-wallet'
     )
+    local number_of_repos=${#a_url[@]}
+
+    echo "Scanning "$number_of_repos "repositories."
+    print_separator
+
     for u in ${a_url[@]}
     do
          scan_repo $u
+         ((i++))
+         echo "Scanned" $i"/"$number_of_repos "repositories."
+         print_separator
     done
+}
+
+function main() {
+
+    iterate_over_repos
+
 }
 
 main
